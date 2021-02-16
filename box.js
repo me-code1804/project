ab6f1895ac9e19be30dd7afe51e8c4e072371f09
me-code1804 <78306608+me-code1804@users.noreply.github.com>
2021-02-16 19:47:26 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,26 @@
+class Box{
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.4,
+            'friction':0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+
+        World.add(world, this.body);
+      }
+      display(){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rectMode(CENTER);
+        strokeWeight(2);
+        stroke(0);
+        this.Visiblity = this.Visiblity - 5;
+     tint(255,this.Visiblity);
+     image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+     pop();
+        rect(0, 0, this.width, this.height);
+        pop();
+      }
+}
